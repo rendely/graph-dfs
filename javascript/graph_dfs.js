a8438fa256b9822a1a62759f944391a93597cf0b
matthew rendely <matthew.rendely@gmail.com>
2023-12-05 21:18:21 -0800

    done

--- javascript/graph_dfs.js
+++ javascript/graph_dfs.js
@@ -1,9 +1,19 @@
 function isPath(graph, vertexA, vertexB) {
-  // type your code here
+  // console.log(vertexA, vertexB);
+  if (!graph[vertexA]) return false;
+  if (graph[vertexA].indexOf(vertexB) >= 0) return true;
+
+  let isAdjacent = false;
+  for (vertex of graph[vertexA]){
+    const newGraph = {...graph}
+    delete newGraph[vertexA];
+    if (isPath(newGraph, vertex, vertexB)) isAdjacent = true;
+  }
+  return isAdjacent;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  // // add your own tests in here
   let graph = {
     jan: ["john", "jambaby"],
     john: ["carl"],
@@ -12,16 +22,37 @@ if (require.main === module) {
     dave: []
   };
 
-  console.log("Expecting: true");
-  console.log(isPath(graph, "jan", "carl"));
+  // console.log("Expecting: true");
+  // console.log(isPath(graph, "jan", "john"));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: false");
-  console.log(isPath(graph, "jan", "dave"));
+  // console.log("Expecting: true");
+  // console.log(isPath(graph, "jan", "carl"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log(isPath(graph, "jan", "dave"));
+
+  const lessSimpleGraph = {
+    jan: ["john", "jambaby"],
+    john: ["carl"],
+    jambaby: [],
+    carl: ["jambaby", "dave"],
+    dave: ["jan"],
+    mittens: []
+  };
+
+  console.log(isPath(graph, 'jan', 'jambaby'))
 }
 
 module.exports = isPath;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// First check if vertexB is adjacent to vertexA
+// If not check if any of the vertices connected to vertexA are adjacent to B
+// If not check if any of the vertices connected to vertices connected to A are adjacent to B
+// Continue until all nodes checked
